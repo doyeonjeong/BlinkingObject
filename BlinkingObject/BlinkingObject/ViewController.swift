d7cd7610094d58501e84c41abb4c8fc9e7c2f334
doyeonjeong <debby_@kakao.com>
2023-03-03 17:31:13 +0900

    ✨ :: Add Flickering Animation function

--- BlinkingObject/BlinkingObject/ViewController.swift
+++ BlinkingObject/BlinkingObject/ViewController.swift
@@ -15,12 +15,31 @@ class ViewController: UIViewController {
         self.view.addSubview(drawObject(x: 100, y: 200, width: 200, height: 200))
     }
     
+    /// Returns a UIView object with the specified position, size, and background color, and adds a flickering animation to it.
+    /// - Parameters:
+    ///   - x: The x-coordinate of the top-left corner of the view.
+    ///   - y: The y-coordinate of the top-left corner of the view.
+    ///   - width: The width of the view.
+    ///   - height: The height of the view.
+    /// - Returns: A UIView object with the specified position, size, and background color, and a flickering animation.
     func drawObject(x: Int, y: Int, width: Int, height: Int) -> UIView {
+        // Create a new UIView object with the specified position, size, and background color
         let object = UIView()
         object.frame = CGRect(x: x, y: y, width: width, height: height)
         object.backgroundColor = UIColor(red: 0.93, green: 0.94, blue: 0.95, alpha: 1.00)
         object.layer.cornerRadius = 20
+        
+        // Add a flickering animation to the object
+        addBlinkingAnimation(to: object, withDuration: 2.0, delay: 0.0, minAlpha: 0.5)
+        
         return object
+    }
+
+    
+    func addBlinkingAnimation(to object: UIView, withDuration duration: TimeInterval, delay: TimeInterval, minAlpha: CGFloat) {
+        UIView.animate(withDuration: duration, delay: delay, options: [.repeat, .autoreverse]) {
+            object.alpha = minAlpha
+        }
     }
 
 }
